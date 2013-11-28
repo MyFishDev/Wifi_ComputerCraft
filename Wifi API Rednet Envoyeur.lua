5506dc096e388b0712859cfa2717c26582a27295
MyFishDev <equipe@myfishdev.url.ph>
2013-11-28 19:16:34 +0100

    Update Wifi API Rednet Envoyeur.lua

--- Wifi API Rednet Envoyeur.lua
+++ Wifi API Rednet Envoyeur.lua
@@ -1,3 +1,9 @@
-rednet.open("top")
+-- side doit etre remplace par le cote sur lequel est place le modem
+-- ID doit etre remplace par l'id du l'ordi sur lequel va etre envoye le message
+-- message doit etre remplace par le message a envoyer
+rednet.open("side")
+rednet.send(ID,"message")
+-- OU
+rednet.open("side")
 texte = read()
-rednet.send(21,mot)
+rednet.send(ID,texte)
