19dadfc6d68adffa916cea1083720d7ed50fab25
MyFishDev <equipe@myfishdev.url.ph>
2013-11-28 20:41:07 +0100

    Update Wifi API Modem Recepteur.lua

--- Wifi API Modem Recepteur.lua
+++ Wifi API Modem Recepteur.lua
@@ -1,1 +1,5 @@
-local
+-- remplacez side par le cote sur lequel vous avez place le modem
+-- remplacez freq par la frequence sur voulez recevoir
+local modem = peripheral.wrap("side")
+modem.open("freq")
+local _, side, freq, rfreq, message = os.pullEvent('modem_message')
